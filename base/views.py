4cba6299d137d716ef0059782f64265d686de860
MrSupremeX <ssahajjot@gmail.com>
2025-03-02 18:57:34 +1100

    Bug Fixes
    - In the top left corner, the logout link was not working, after modifying the code and updating the link system to a button, it worked.
    -Users were able to view/edit/delete each other's tasks but the issue has been fixed and they cannot view or edit or delete anyone's task but their own.

--- base/views.py
+++ base/views.py
@@ -43,18 +43,18 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)  # Only show the logged-in user's tasks
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
 
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
-            context['tasks'] = context['tasks'].filter(
-                title__icontains=search_input)
-         
+            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+
         context['search_input'] = search_input
-        
         return context
 
 
@@ -82,8 +82,14 @@ class TaskUpdate(LoginRequiredMixin, UpdateView):
     fields = ['title', 'description', 'complete']
     success_url =reverse_lazy('tasks')
 
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)  # Prevents users from editing other users' tasks
+
 
 class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url =reverse_lazy('tasks')
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)  # Prevents users from editing other users' tasks
